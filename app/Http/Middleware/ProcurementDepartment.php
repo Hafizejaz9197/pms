781619fadc0042888b19e3b769830af4c5d2c7bd
Hafizejaz9197 <67779199+Hafizejaz9197@users.noreply.github.com>
2023-01-01 19:17:51 +0500

    Update ProcurementDepartment.php

--- app/Http/Middleware/ProcurementDepartment.php
+++ app/Http/Middleware/ProcurementDepartment.php
@@ -18,16 +18,16 @@ class ProcurementDepartment
     public function handle(Request $request, Closure $next)
     {
         //dd('hello');
-        if (Auth::user() &&  Auth::user()->role == '2') {
+        if (Auth::user() &&  Auth::user()->role == '1') {
             //dd('hello');
             return $next($request);
         }
         else {
-            //return redirect()->intended('login');
-            Auth::logout();
-            $request->session()->invalidate();
-            $request->session()->regenerateToken();
-            return redirect('/login');
+            return redirect()->intended('login');
+            //Auth::logout();
+            //$request->session()->invalidate();
+            //$request->session()->regenerateToken();
+            //return redirect('/login');
             
         }
         //dd('hello after if');
