e64bedb6d0547ccaf2c58c8d7b7fbae29776bb64
Hafizejaz9197 <67779199+Hafizejaz9197@users.noreply.github.com>
2023-01-02 15:41:41 +0500

    ffdsg

--- tests/mytest.php
+++ tests/mytest.php
@@ -1,15 +1,6 @@
 <?php 
 
-echo "Test File";
-echo "Test File2";
-
-echo "Test File223243324";
-
-echo "Test file This is not done";
-
-echo "Now it is new test tesdfsadfdsagfsd";
-
-echo "We are exprienceiung the command line interfacesd dsaf sadf sdf sadf.";
+echo "Final Test";
 
 ?>
  
